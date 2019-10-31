cd42f8a804427221f2d90401338267794a23344c
JuPillet <juju.pillet@hotmail.com>
2019-10-31 17:32:19 +0100

    new try export prod

--- src/store/vars_route.js
+++ src/store/vars_route.js
@@ -1,5 +1,5 @@
 // depuis le serveur aws
-//export const baseUri = 'ec2-3-82-197-202.compute-1.amazonaws.com/RunBack/public';
+//export const baseUri = 'RunBack/public';
 
 
 // depuis le vpn de Brice
