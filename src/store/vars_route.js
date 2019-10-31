6c7cb2861b3d658d6ddf9e0148bb62902bc0ecb8
JuPillet <juju.pillet@hotmail.com>
2019-10-31 13:37:00 +0100

    for the prod

--- src/store/vars_route.js
+++ src/store/vars_route.js
@@ -1,15 +1,16 @@
 // depuis le serveur aws
-// export const baseUri = 'ec2-3-82-197-202.compute-1.amazonaws.com/RunBack/public';
+//export const baseUri = 'ec2-3-82-197-202.compute-1.amazonaws.com/RunBack/public';
 
 
 // depuis le vpn de Brice
-export const baseUri = 'http://brice-ouaali.vpnuser.lan/projet-Runmap/public';
+//export const baseUri = 'http://brice-ouaali.vpnuser.lan/projet-Runmap/public';
 
 // depuis le vpn de Julien
 // export const baseUri = 'http://julien-pillet.vpnuser.lan/Apotheose/RunBack/public';
 
 // depuis mon localhost (JC)
-// export const baseUri = 'http://localhost/projet-Runmap/public';
+export const baseUri = 'http://localhost/projet-Runmap/public';
+
 // Routes
 export const registerRoute = '/register';
 
