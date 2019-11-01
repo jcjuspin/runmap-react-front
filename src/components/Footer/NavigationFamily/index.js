f40ea88f50fc47bbe25f0e95b961fea9dd08611a
github_brice13750 <brice.ouaali@gmail.com>
2019-11-01 14:31:56 +0100

    mise en place lien footer

--- src/components/Footer/NavigationFamily/index.js
+++ src/components/Footer/NavigationFamily/index.js
@@ -13,8 +13,8 @@ const NavigationFamily = () => {
       <ul className="list-nav"> 
         <li> <NavLink to="/inscription" exact>S'inscrire</NavLink> </li>
         <li> <NavLink to="/contact" exact>Nous contacter</NavLink> </li>
-        <li> <NavLink to="/contact" exact>Ajouter une ville</NavLink> </li>
-        <li> <NavLink to="/contact" exact>Ajouter un lieu</NavLink> </li>
+        <li> <NavLink to="/ajouter" exact>Ajouter une ville</NavLink> </li>
+        <li> <NavLink to="/ajouter" exact>Ajouter un lieu</NavLink> </li>
       </ul>
     </>
   )
