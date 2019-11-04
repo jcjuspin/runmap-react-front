4e10f59e072950572b90b8e31491c856bd98d141
Mig-Robot <miguel.oclockuniverse@gmail.com>
2019-11-04 17:22:47 +0100

    Ajout Img

--- src/components/Page/AProposPage/index.js
+++ src/components/Page/AProposPage/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Card, CardColumns } from 'react-bootstrap';
 
+import './aProposPage.scss';
+
 const AProposPage = () => (
 
   <div className="container mt-2 overflow-auto">
@@ -22,12 +24,16 @@ const AProposPage = () => (
   </Card>
 
       <Card>
-        <Card.Img variant="top" src="src/components/Page/AProposPage/coderr.jpg" />
+        <Card.Img variant="top" src="src/components/Page/AProposPage/Kevin.jpg" />
         <Card.Body>
-          <Card.Title><span className="text-monospace">Kevin : Lead DevBack.</span></Card.Title>
+          <Card.Title>
+            <span className="text-monospace">Kevin : Lead DevBack.</span>
+            <div className="">
+              <p className="text-muted text-descript">Accessoirement le plus sérieux mais pas le dernier pour sortir un vanne !</p>
+            </div>
+            </Card.Title>
           <Card.Text>
-            This is a longer card with supporting text below as a natural lead-in to
-            additional content. This content is a little bit longer.
+            -
           </Card.Text>
         </Card.Body>
       </Card>
@@ -45,13 +51,56 @@ const AProposPage = () => (
         </blockquote>
       </Card>
 
+      <Card className="text-center p-3">
+        <blockquote className="blockquote mb-0 card-body">
+          <p>
+            On ne te demande pas de montrer ta lune MDR.
+          </p>
+          <footer className="blockquote-footer">
+            <small className="text-muted">
+              Kevin - <cite title="Source Title">Pour motiver Julien à envoyé une photo</cite>
+            </small>
+          </footer>
+        </blockquote>
+      </Card>
+
+      <Card className="text-center p-3">
+        <blockquote className="blockquote mb-0 card-body">
+          <p>
+            Travailler avec moi, c'est comme avoir fait le Vietnam.
+          </p>
+          <footer className="blockquote-footer">
+            <small className="text-muted">
+              Julien - <cite title="Source Title">Quand on galère à le comprendre</cite>
+            </small>
+          </footer>
+        </blockquote>
+      </Card>
+
+      <Card className="text-center p-3">
+        <blockquote className="blockquote mb-0 card-body">
+          <p>
+            <b>Les gars</b> j'ai tout perdu, j'ai une page blanche maintenant ...
+          </p>
+          <footer className="blockquote-footer">
+            <small className="text-muted">
+              Miguel - <cite title="Source Title">qui avait encore oublié de faire un git pull sur master</cite>
+            </small>
+          </footer>
+        </blockquote>
+      </Card>
+
       <Card>
-        <Card.Img variant="top" src="src/components/Page/AProposPage/coderr.jpg" />
+        <Card.Img variant="top" src="src/components/Page/AProposPage/Brice.jpg" />
         <Card.Body>
-        <Card.Title><span className="text-monospace">Brice : Trello, c'est mon crénaux</span></Card.Title>
+          <Card.Title>
+            <span className="text-monospace">Brice : Trello, c'est mon crénaux</span>
+            <div className="">
+              <p className="text-muted text-descript">C'est lui après avoir trouvé la solution au problèmes qui l'a bloqué pendant 3 jours ...</p>
+            </div>
+          </Card.Title>
           <Card.Text>
-            This is a longer card with supporting text below as a natural lead-in to
-            additional content. This content is a little bit longer.
+            -
           </Card.Text>
         </Card.Body>
       </Card>
@@ -72,10 +121,13 @@ const AProposPage = () => (
       <Card>
         <Card.Img variant="top" src="src/components/Page/AProposPage/coderr.jpg" />
         <Card.Body>
-        <Card.Title><span className="text-monospace">Miguel : Intégration ? Quelqu'un à dit intégration ? </span></Card.Title>
+          <Card.Title><span className="text-monospace">Miguel : Project Owner .. ASKIP!</span>
+            <div className="">
+              <p className="text-muted text-descript">Il en avait marre de payer une licence, alors il a voulu créer RunMap !</p>
+            </div>
+          </Card.Title>
           <Card.Text>
-            This is a longer card with supporting text below as a natural lead-in to
-            additional content. This content is a little bit longer.
+            -
           </Card.Text>
         </Card.Body>
       </Card>
@@ -94,24 +146,45 @@ const AProposPage = () => (
       </Card>
 
       <Card>
-        <Card.Img variant="top" src="src/components/Page/AProposPage/coderr.jpg" />
+        <Card.Img variant="top" src="src/components/Page/AProposPage/Julien.jpg" />
         <Card.Body>
-        <Card.Title><span className="text-monospace">Julien : Alias, GitPush</span></Card.Title>
+          <Card.Title>
+            <span className="text-monospace">Julien : Alias, GitPush</span>
+            <div className="">
+              <p className="text-muted text-descript">Apparement il est de la famille de 'Tout EnCarton" c'est lui qui le dit ..</p>
+            </div>
+          </Card.Title>
           <Card.Text>
-            This is a longer card with supporting text below as a natural lead-in to
-            additional content. This content is a little bit longer.
+            -
           </Card.Text>
         </Card.Body>
+      </Card>
+
+      <Card className="text-center p-3">
+        <blockquote className="blockquote mb-0 card-body">
+          <p>
+            <b>Miguel</b>, tu mettras la meme photo que Kevin pour moi.
+          </p>
+          <footer className="blockquote-footer">
+            <small className="text-muted">
+              JC - <cite title="Source Title">Au moment de créer cette page</cite>
+            </small>
+          </footer>
+        </blockquote>
       </Card>
       
 
       <Card>
-        <Card.Img variant="top" src="src/components/Page/AProposPage/coderr.jpg" />
+        <Card.Img variant="top" src="src/components/Page/AProposPage/JeanChristophe.png" />
         <Card.Body>
-        <Card.Title><span className="text-monospace">JC : LeadDev Front Infiltré</span></Card.Title>
+          <Card.Title>
+            <span className="text-monospace">JC : LeadDev Front Infiltré</span>
+            <div className="">
+              <p className="text-muted text-descript">Le Master Map, des jours de galères mais il y est arrivé !</p>
+            </div>
+          </Card.Title>
           <Card.Text>
-            This is a longer card with supporting text below as a natural lead-in to
-            additional content. This content is a little bit longer.
+            -
           </Card.Text>
         </Card.Body>
       </Card>
