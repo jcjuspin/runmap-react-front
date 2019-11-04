daa10f6bdab3e7254667f6717be252ce03c71a28
github_brice13750 <brice.ouaali@gmail.com>
2019-11-04 13:07:10 +0100

    intégration user-admin

--- src/components/Page/AdminPage/index.js
+++ src/components/Page/AdminPage/index.js
@@ -1,19 +1,44 @@
 // == Import : npm
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 
 // == Import : local
 import './adminpage.scss';
 // == Composant
-const AdminPage = () => {
-  
-  return (
-    <>
-      <h1>Je suis la page admin</h1>
-    </>
-  );
-};
+const AdminPage = () => (
+  <>
+    <h1>Utilisateur</h1>
+    <div className="container">
+      <div className="jumbotron">
+        <div className="row">
+          <div className="col-lg-5">
+            <p>Email: brice.ouaali@gmail.com</p>
+            <hr />
+            <p>Nom: OUAALI </p>
+            <hr />
+            <p>Prénom: Brice</p>
+          </div>
+
+          <div className="col-lg-5">
+            <p>id: 1</p>
+            <hr />
+            <p>Genre: Homme</p>
+            <hr />
+            <p>Age: 24 ans</p>
+          </div>
+        
+          <div className="col-lg-2">
+            <p>Supprimer</p>
+            <hr />
+            <a href="#">
+              <img className="img-admin" src="src/components/Page/AdminPage/trash.svg" alt="trash" /> 
+            </a>
+          </div>
+        </div>  
+      </div>
+    </div>
+  </>
+);
 
 AdminPage.propTypes = {
 
