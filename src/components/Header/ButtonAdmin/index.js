66b50efec88408ce572e34babc539420bbf50ac7
Mig-Robot <miguel.oclockuniverse@gmail.com>
2019-11-06 16:19:47 +0100

    NavLink sur btn + CSS

--- src/components/Header/ButtonAdmin/index.js
+++ src/components/Header/ButtonAdmin/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NavLink } from 'react-router-dom';
+
+import './buttonAdmin.scss';
 
 const ButtonAdmin = () => (
 
   <button type="button" className="btn btn-danger ml-1">
-    <FontAwesomeIcon icon="user-cog" /> <b>Admin</b>
+    <NavLink className="admin-lien" to="/admin" exact>
+      <FontAwesomeIcon icon="user-cog" /> <b>Admin</b>
+    </NavLink>
   </button>
 
 );
