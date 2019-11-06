806f4bda36729309a78fedf63c6b12d8838e8ccb
Mig-Robot <miguel.oclockuniverse@gmail.com>
2019-11-06 16:42:57 +0100

    Amélioration Dropdown

--- src/components/Header/DropdownHeader/index.js
+++ src/components/Header/DropdownHeader/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const DropdownHeader = () => (
-  <DropdownButton
-    alignRight
-    title=<FontAwesomeIcon icon="user" />
-    variant="warning"
-    id="dropdown-menu-align-right"
-  >
-    <Dropdown.Item eventKey="1"><NavLink to="/ajouter" exact>Ajouter un lieu</NavLink></Dropdown.Item>
-    <Dropdown.Item eventKey="2"><NavLink to="/contact" exact>Modifier un lieu</NavLink></Dropdown.Item>
-    <Dropdown.Item eventKey="3"><NavLink to="/user" exact>Profil</NavLink></Dropdown.Item>
-    <Dropdown.Divider />
-    <Dropdown.Item eventKey="4"><NavLink to="/" exact>Deconnexion</NavLink></Dropdown.Item>
-  </DropdownButton>
+  <Dropdown>
+    <Dropdown.Toggle variant="warning">
+      <FontAwesomeIcon icon="user" /> <b>Member</b>
+    </Dropdown.Toggle>
+    <Dropdown.Menu>
+      <Dropdown.Item eventKey="1"><NavLink to="/ajouter" exact>Ajouter un lieu</NavLink></Dropdown.Item>
+      <Dropdown.Item eventKey="2"><NavLink to="/contact" exact>Modifier un lieu</NavLink></Dropdown.Item>
+      <Dropdown.Item eventKey="3"><NavLink to="/user" exact>Profil</NavLink></Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item eventKey="4"><NavLink to="/" exact>Deconnexion</NavLink></Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
 );
 
 export default DropdownHeader;
