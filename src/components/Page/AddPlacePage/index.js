39f40bf7345285c8f8f925343f794af026cb1664
Mig-Robot <miguel.oclockuniverse@gmail.com>
2019-11-06 14:15:02 +0100

    Form AddPlace : Done

--- src/components/Page/AddPlacePage/index.js
+++ src/components/Page/AddPlacePage/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 // == Import : local
-// import './inscriptionpage.scss';
+import './addPlacePage.scss';
 // == Composant
 // TODO: par sécurité il faudrait que le formulaire affiche ou réactive les inputs en fonction de la ville tappée etc (avec info recuperé par l'api du gouvernement)
 // TODO: mettre en place une autocomplétion qui va chercher dans la base de donnée si le stade existe déja. Si il existe déja, une infos qui prévient et faut bloquer le formulaire.
@@ -40,146 +40,152 @@ const AddPlacePage = ({
   };
 
   return (
-    <div className="container-fluid register-body">
-      <div className="container-fluid container-page">
-        <div className="container container-panel registration-panel">
 
-          <div className="panel-title">
-            <span>Ajoute un lieu</span>
+      <div className="container-fluid addPlace-page">
+        <div className="row">
+          <div className="col-sm addPlace-img">
+            <div className="addPlace-background">
+              <div className="addPlace-title">
+                <h2>Et un nouveau terrain de jeu, un !</h2>
+              </div>
+            </div>
           </div>
-
-          {/* ----------------- FORMULAIRE POUR AJOUTER UN LIEU ------------------ */}
-          <form onSubmit={handleSubmit} className="order signup website">
-            <fieldset className="user-inputs mt-0 mb-0">
-
-              {/* VILLE */}
-              <div className="form-group">
-
-              <label class="text-warning" for="city">Ville</label>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="ville"
-                  id="city-name"
-                  name="cityName"
-                  value={addPlaceFormData.cityName}
-                  onChange={handleChange}
-                  // required
-                />
+          <div className="col-sm addPlace-pannel">
+            {/* ----------------- FORMULAIRE POUR AJOUTER UN LIEU ------------------ */}
+            <form onSubmit={handleSubmit} className="order container addPlace-form">
+              <fieldset className="addPlace-user-inputs mt-0 mb-0">
+    
+                {/* NOM DU LIEU */}
+                <div className="form-group addPlace-form-group">
+    
+                <label className="text-warning" for="name">Nom du lieu</label>
+                  <input
+                    className="form-control addPlace-form-control"
+                    type="text"
+                    placeholder="Stade John Doe"
+                    id="place-name"
+                    name="placeName"
+                    value={addPlaceFormData.placeName}
+                    onChange={handleChange}
+                    // required
+                  />
+                </div>
+    
+                {/* VILLE */}
+                <div className="form-group addPlace-form-group">
+    
+                <label className="text-warning" for="city">Ville</label>
+                  <input
+                    className="form-control addPlace-form-control"
+                    type="text"
+                    placeholder="Paris"
+                    id="city-name"
+                    name="cityName"
+                    value={addPlaceFormData.cityName}
+                    onChange={handleChange}
+                    // required
+                  />
+                </div>
+    
+                {/* CODE POSTALE */}
+                <div className="form-group addPlace-form-group">
+    
+                <label class="text-warning" for="postal-code">Code postal</label>
+                  <input
+                    className="form-control addPlace-form-control"
+                    type="text"
+                    placeholder="75000"
+                    id="postal-code"
+                    name="postalCode"
+                    value={addPlaceFormData.postalCode}
+                    onChange={handleChange}
+                    // required
+                  />
+                </div>
+    
+                {/* ADRESSE DU LIEU */}
+                <div className="form-group addPlace-form-group">
+    
+                <label class="text-warning" for="adress">Adresse</label>
+                  <input
+                    className="form-control addPlace-form-control"
+                    type="text"
+                    placeholder="Rue Agent Secret"
+                    id="adress"
+                    name="adress"
+                    value={addPlaceFormData.adress}
+                    onChange={handleChange}
+                    // required
+                  />
+                </div>
+    
+                {/* LES HORAIRES */}
+                <div className="form-group addPlace-form-group">
+    
+                <label className="text-warning" for="schedule">Horaires</label>
+                  <textarea
+                    className="form-control addPlace-form-control"
+                    type="text"
+                    rows="3"
+                    placeholder="Jours et crenaux horaires d'ouverture et fermeture"
+                    id="place-schedule"
+                    name="schedule"
+                    value={addPlaceFormData.schedule}
+                    onChange={handleChange}
+                    // required
+                  />
+                </div>
+    
+                {/* COMPLEMENTS D'INFOS */}
+                <div className="form-group addPlace-form-group">
+    
+                <label className="text-warning" for="information">Informations</label>
+                  <textarea
+                    className="form-control addPlace-form-control"
+                    type="text"
+                    rows="3"
+                    placeholder="Tout ce qui peux vous semblez utile à rajouter."
+                    id="additional-information"
+                    name="additionalInfo"
+                    value={addPlaceFormData.additionalInfo}
+                    onChange={handleChange}
+                    // required
+                  />
+                </div>
+    
+              </fieldset>
+    
+              {/* BOUTTON SUBMIT */}
+              <div className="btn-login">
+                <button type="submit" className="button-signup">C'est parti</button>
               </div>
-
-              {/* CODE POSTALE */}
-              <div className="form-group">
-
-              <label class="text-warning" for="postal-code">Code postal</label>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="code postal"
-                  id="postal-code"
-                  name="postalCode"
-                  value={addPlaceFormData.postalCode}
-                  onChange={handleChange}
-                  // required
-                />
+              {/* LIENS CHANGEMENT DE PAGE */}
+              <div className="disclaimer-text">
+                <p>Retour à la <Link to="/">page d'accueil</Link></p>
+                <p>Pour plus d'information <Link to="/contact">contactez-nous</Link></p>
               </div>
-
-              {/* NOM DU LIEU */}
-              <div className="form-group">
-
-              <label class="text-warning" for="name">Nom du lieu</label>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="nom du lieu"
-                  id="place-name"
-                  name="placeName"
-                  value={addPlaceFormData.placeName}
-                  onChange={handleChange}
-                  // required
-                />
-              </div>
-
-              {/* ADRESSE DU LIEU */}
-              <div className="form-group">
-
-              <label class="text-warning" for="adress">Adresse</label>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="adresse du lieu"
-                  id="adress"
-                  name="adress"
-                  value={addPlaceFormData.adress}
-                  onChange={handleChange}
-                  // required
-                />
-              </div>
-
-              {/* LES HORAIRES */}
-              <div className="form-group">
-
-              <label class="text-warning" for="schedule">Horaire</label>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="les horaires"
-                  id="place-schedule"
-                  name="schedule"
-                  value={addPlaceFormData.schedule}
-                  onChange={handleChange}
-                  // required
-                />
-              </div>
-
-              {/* COMPLEMENTS D'INFOS */}
-              <div className="form-group">
-
-              <label class="text-warning" for="information">Informations</label>
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="complément d'informations"
-                  id="additional-information"
-                  name="additionalInfo"
-                  value={addPlaceFormData.additionalInfo}
-                  onChange={handleChange}
-                  // required
-                />
-              </div>
-
-            </fieldset>
-
-            {/* BOUTTON SUBMIT */}
-            <div className="btn-login">
-              <button type="submit" className="button-signup">C'est parti</button>
-            </div>
-            {/* LIENS CHANGEMENT DE PAGE */}
-            <div className="disclaimer-text">
-              <p>Retour à la <Link to="/">page d'accueil</Link></p>
-              <p>Pour plus d'information <Link to="/contact">contactez-nous</Link></p>
-            </div>
-            {validationMessage && (
-              <div className="alert alert-success" role="alert">
-                {validationMessage}
-              </div>
-            )}
-            { errorMessageAlert && (
+              {validationMessage && (
+                <div className="alert alert-success" role="alert">
+                  {validationMessage}
+                </div>
+              )}
+              { errorMessageAlert && (
+                <div className="alert alert-danger" role="alert">
+                  {errorMessageAlert}
+                </div>
+              )}
+              {/* message d'erreur si le mot de passe n'est pas correct */}
+              {displayPasswordErrorMessage && (
               <div className="alert alert-danger" role="alert">
-                {errorMessageAlert}
+              Le mot de passe ne correspond pas. Veuillez corriger!
               </div>
-            )}
-            {/* message d'erreur si le mot de passe n'est pas correct */}
-            {displayPasswordErrorMessage && (
-            <div className="alert alert-danger" role="alert">
-            Le mot de passe ne correspond pas. Veuillez corriger!
-            </div>
-            )}
-          </form>
-
+              )}
+            </form>
+    
+          </div>
         </div>
       </div>
-    </div>
+  
   );
 };
 
